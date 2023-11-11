9d882fe91306189599a68cadf8d3b2a55a6196e9
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2023-11-11 13:52:33 +0100

    Upgrade PDF Creation

---
+++ Manga/MangaPDF.py
@@ -0,0 +1,8 @@
+from Modules import PDFUtils
+import os
+
+directory = os.path.dirname(os.path.abspath(__file__))
+manga = "Neon Genesis Evangelion"
+division = "Stage"
+
+PDFUtils.preparationForPDF(manga,directory,division)
